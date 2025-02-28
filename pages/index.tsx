b2f0ab9d3427ea5655abe03f5d83d2b2c80bc720
GMblong <andiarifrahmatullah@gmail.com>
2025-02-28 14:34:22 +0700

    cb

--- pages/index.tsx
+++ pages/index.tsx
@@ -129,7 +129,7 @@ const index: React.FC<indexProps> = () => {
             name="apple-mobile-web-app-status-bar-style"
             content="#10101A"
           />
-          <title>Adeola Adeoti 🚀 &mdash; Frontend Devloper</title>
+          <title>Erick Nurdiyanto 🚀 &mdash; IT Support</title>
           <meta
             name="description"
             content="I'm a self-taught Front End Developer and turning ideas into real life products is my calling."
